01b9d0c8562b7de9ff4c576c3193197b1543f52b
Sam Ivanov <si@juno-solar.com>
2024-02-04 12:05:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolarPowerEngineering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter words which are separated by space. Press Enter when done:");
+            var inputWords = Console.ReadLine().Split(' ');
+
+            var filteredWords = StringFilter.FilterWordsStartingWithA(inputWords);
+
+            Console.WriteLine("Words which starts with 'A':");
+            foreach (var word in filteredWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ StringFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SolarPowerEngineering
+{
+    public static class StringFilter
+    {
+        public static IEnumerable<string> FilterWordsStartingWithA(IEnumerable<string> words)
+        {
+            return words.Where(word => word.StartsWith("A", StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
